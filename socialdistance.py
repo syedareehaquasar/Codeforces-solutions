a9b72e3e9bbd18573cac31c7a524e4e13e9a2e82
syedareehaquasar <reeha.saba@gmail.com>
2020-06-18 20:15:51 +0530

    solving problemset watermelon

---
+++ socialdistance.py
@@ -0,0 +1,9 @@
+def solution(s : str, k : int) -> int:
+    if s = 
+
+
+tc = int(input())
+for i in range(tc):
+    n, k = [int(x) for x in input().split()]
+    s = input()
+    print(solution(s, k))
