849e4c75d57bfe6db3c74872d32b0275a12383a4
syedareehaquasar <reeha.saba@gmail.com>
2020-06-18 22:50:10 +0530

    codefoces

--- socialdistance.py
+++ socialdistance.py
@@ -1,9 +1,11 @@
-def solution(s : str, k : int) -> int:
-    if s = 
-
+def solution(s : str, k : int, n : int) -> int:
+    for i, v in enumerate(s):
+        if v == 1:
+            if i > k:
+                ans += 
 
 tc = int(input())
 for i in range(tc):
     n, k = [int(x) for x in input().split()]
     s = input()
-    print(solution(s, k))
+    print(solution(s, k, n))
